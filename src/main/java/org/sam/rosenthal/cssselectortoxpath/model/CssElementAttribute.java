d885b2d9ba68ac929d0b9d0a3b87483edf524b0b
sam-rosenthal <ser259@cornell.edu>
2018-06-10 12:35:09 -0400

    Added CssElementAttribute and CssElementAttributeParser

---
+++ src/main/java/org/sam/rosenthal/cssselectortoxpath/model/CssElementAttribute.java
@@ -0,0 +1,36 @@
+package org.sam.rosenthal.cssselectortoxpath.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.sam.rosenthal.cssselectortoxpath.utilities.CssSelectorStringSplitterException;
+//Take a string and the format of the string is...
+//xxx[yyy]...[zzz]
+//-->break it up into a list = xxx,[yyy],..,[zzz]
+//if xxx DNE==>break into list=*,[yyy],...,[zzz]
+//
+//create new class
+//make into css element attribute
+//fills this stuff,
+//only has constructor and getters
+public class CssElementAttribute
+{
+	private String element;
+	private List<String> attributeList;
+
+	public CssElementAttribute(String elementIn, List<String> attributeListIn) throws CssSelectorStringSplitterException
+	{
+		this.element=elementIn;
+		this.attributeList=attributeListIn;
+	}
+
+	public String getElement() {
+		return element;
+	}
+
+	public List<String> getAttributeList() {
+		return attributeList;
+	}
+}
---
+++ src/main/java/org/sam/rosenthal/cssselectortoxpath/utilities/CssElementAttributeParser.java
@@ -0,0 +1,55 @@
+package org.sam.rosenthal.cssselectortoxpath.utilities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CssElementAttributeParser 
+{
+	private static final String STARTING_ELEMENT_REGULAR_EXPRESSION = "^([^\\[\\]])*";
+	private static final String ATTRIBUTE_REGULAR_EXPRESSION = "(\\[[^\\[\\]]+\\])";
+
+	public void checkValid(String elementIn) throws CssSelectorStringSplitterException
+	{
+		Pattern pattern1 = Pattern.compile(STARTING_ELEMENT_REGULAR_EXPRESSION+ATTRIBUTE_REGULAR_EXPRESSION+"*$");
+		Matcher match = pattern1.matcher(elementIn);
+		if (match.find()==false)
+		{
+			throw new CssSelectorStringSplitterException("invalid elementInput");
+		}
+		else
+		{
+			System.out.println("Valid");
+		}
+	}
+	
+	public List<String> createAttributeList(String elementWithAttributesString) 
+	{
+		Pattern pattern2 = Pattern.compile(STARTING_ELEMENT_REGULAR_EXPRESSION);
+		Matcher match = pattern2.matcher(elementWithAttributesString);
+		List<String> builder=new ArrayList<String>();
+		String element=null;
+		if (match.find())
+		{
+			String possibleElement = match.group();
+			if(!possibleElement.isEmpty())
+			{
+				element=possibleElement;
+				System.out.println(possibleElement);
+			}
+		}
+		builder.add(element);
+
+		Pattern pattern3 = Pattern.compile(ATTRIBUTE_REGULAR_EXPRESSION);
+		match = pattern3.matcher(elementWithAttributesString);
+		while(match.find())
+		{
+			builder.add(match.group());
+			System.out.println(builder);
+		}	
+		return builder;
+	}
+	
+
+}
