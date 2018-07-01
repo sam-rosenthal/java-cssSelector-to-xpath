54332812a41b2b78d3a54e5fffeab0caaf17a60d
sam-rosenthal <ser259@cornell.edu>
2018-07-01 15:16:03 -0400

    Added attribute value handling without quotes

--- src/test/java/org/sam/rosenthal/cssselectortoxpath/utilities/CssElementCombinatorPairsToXpathTest.java
+++ src/test/java/org/sam/rosenthal/cssselectortoxpath/utilities/CssElementCombinatorPairsToXpathTest.java
@@ -79,11 +79,14 @@ public class CssElementCombinatorPairsToXpathTest
 		
 		testConvertCssStringToXpathString("A#B","//A[@id=\"B\"]");
 		testConvertCssStringToXpathString("A[B=\"C\"]","//A[@B=\"C\"]");
+		testConvertCssStringToXpathString("A[B=C]","//A[@B=\"C\"]");
+		
 		testConvertCssStringToXpathString("A[B^=\"C\"]","//A[starts-with(@B,\"C\")]");
 		testConvertCssStringToXpathString("A[B*=\"C\"]","//A[contains(@B,\"C\")]");	
 		testConvertCssStringToXpathString("A[B$=\"C\"]","//A[substring(@B,string-length(@B)-string-length(\"C\")+1)=\"C\"]");	
 		testConvertCssStringToXpathString("A.B","//A[contains(concat(\" \",normalize-space(@class),\" \"),\" B \")]");	
 		testConvertCssStringToXpathString("A[B~=\"C\"]","//A[contains(concat(\" \",normalize-space(@B),\" \"),\" C \")]");	
+
 		//p[starts-with(@me,concat("you",'-'))]
 		testConvertCssStringToXpathString("A[B|=\"C\"]","//A[starts-with(@B,concat(\"C\",\"-\")) or @B=\"C\"]");	
 		testConvertCssStringToXpathString("[rel|=\"alternate\"]","//*[starts-with(@rel,concat(\"alternate\",\"-\")) or @rel=\"alternate\"]");
@@ -95,7 +98,6 @@ public class CssElementCombinatorPairsToXpathTest
 	@Test
 	public void convertComplexCssStringToXpathStringTest() throws CssSelectorStringSplitterException
 	{
-
 		testConvertCssStringToXpathString("A,B,C","(//A)|(//B)|(//C)");
 	
 		testConvertCssStringToXpathString("A B,A>B","(//A//B)|(//A/B)");
@@ -110,8 +112,17 @@ public class CssElementCombinatorPairsToXpathTest
 
 		testConvertCssStringToXpathString("A ~B, A+ B","(//A/following-sibling::B)|(//A/following-sibling::*[1]/self::B)");
 
-		
 		testConvertCssStringToXpathString("A~B,A+B","(//A/following-sibling::B)|(//A/following-sibling::*[1]/self::B)");
+
+		testConvertCssStringToXpathString("A[B^=  C  ]","//A[starts-with(@B,\"C\")]");
+		testConvertCssStringToXpathString("A[B*= C]","//A[contains(@B,\"C\")]");	
+		testConvertCssStringToXpathString("A[B$=C ]","//A[substring(@B,string-length(@B)-string-length(\"C\")+1)=\"C\"]");	
+		
+		testConvertCssStringToXpathString("A[B=  \"C\" ]","//A[@B=\"C\"]");
+		testConvertCssStringToXpathString("A[B=  \"C \" ]","//A[@B=\"C \"]");
+		testConvertCssStringToXpathString("A[B=  \"x   C\" ]","//A[@B=\"x C\"]");
+		testConvertCssStringToXpathString("A[B=  \"x\tC\" ]","//A[@B=\"x C\"]");
+
 
 		testConvertCssStringToXpathString("#B","//*[@id=\"B\"]");
 		testConvertCssStringToXpathString("[B=\"C\"]","//*[@B=\"C\"]");
